f7879809bfceac67ce7041e766c4a57dfc7c2709
Jeonni <69911004+Jeonni@users.noreply.github.com>
2023-04-13 11:50:26 +0900

    [Bronze IV] Title: 주사위 세개, Time: 216 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int max;
+
+        if(a==b && b==c && c==a){
+            System.out.println(10000+(a*1000));
+        }
+        else if(a==b && a!=c || b==c && b!=a || c==a && c!=b){
+            if(a==b){
+                System.out.println(1000+(a*100));
+            }
+            else if(b==c){
+                System.out.println(1000+(b*100));
+            }
+            else{
+                System.out.println(1000+(c*100));
+            }
+        }
+        else{
+            if(a>b && a>c){
+                max = a;
+            }
+            else if(b>a && b>c){
+                max = b;
+            }
+            else{
+                max = c;
+            }
+            System.out.println(max*100);
+        }
+    }
+}
