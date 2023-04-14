a0e7c0a12d2009f0b4755b8641bdb6f4c41079af
Jeonni <69911004+Jeonni@users.noreply.github.com>
2023-04-14 13:52:12 +0900

    [Bronze II] Title: 숫자의 개수, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] list = new int[10];
+
+        int value = Integer.parseInt(br.readLine())*Integer.parseInt(br.readLine())*Integer.parseInt(br.readLine());
+
+        String str = String.valueOf(value);
+
+        for (int i = 0; i < str.length(); i++) {
+            list[(str.charAt(i)-'0')]++;
+        }
+
+        for (int v : list) {
+            System.out.println(v);
+        }
+
+    }
+}
