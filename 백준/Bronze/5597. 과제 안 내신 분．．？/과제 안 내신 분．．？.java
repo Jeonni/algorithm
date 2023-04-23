764ef4bc6c1c02ba5374e15503be1b8b8a9e00e8
Jeonni <69911004+Jeonni@users.noreply.github.com>
2023-04-23 16:49:45 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 124 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,50 +1,19 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        boolean[] isExsist = new boolean[31];
 
-        // 배열에 값 담기 1-30까지
-        int list[] = new int[30];
-        for (int i = 1; i <= list.length; i++) {
-            list[i - 1] = i;
+        for (int i = 0; i < 28; i++) {
+            isExsist[Integer.parseInt(br.readLine())] = true;
         }
 
-        // 비교할 배열에 값 하나씩 할당
-        int compareList[] = new int[28];
-        for (int i = 0; i < compareList.length; i++) {
-            int num = input.nextInt();
-            compareList[i] = num;
-        }
-
-        // 크기 순으로 정렬하기
-        Arrays.sort(list);
-        Arrays.sort(compareList);
-
-        int i = 0;
-        int j = 0;
-
-        // 중복되지 않는 값을 찾아서 출력
-        while (i < list.length && j < compareList.length) {
-            if (list[i] == compareList[j]) {
-                i++;
-                j++;
-            } else if (list[i] < compareList[j]) {
-                System.out.println(list[i]);
-                i++;
-            }
-        }
-
-        while (i < list.length) {
-            System.out.println(list[i]);
-            i++;
-        }
-
-        while (j < compareList.length) {
-            System.out.println(compareList[j]);
-            j++;
+        for (int i = 1; i < 31; i++) {
+            if(!isExsist[i]) System.out.println(i);
         }
     }
 }
