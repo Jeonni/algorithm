07c2d3ebc2980e9e3a2047a94d57f3ed94fe7760
Jeonni <69911004+Jeonni@users.noreply.github.com>
2023-05-14 18:32:22 +0900

    [Silver II] Title: 스택 수열, Time: 352 ms, Memory: 26696 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] list = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            list[i] = Integer.parseInt(br.readLine());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        int stackNum = 1; 
+
+        for (int i = 0; i < n; i++) {
+            while (stackNum <= list[i]) { 
+                stack.push(stackNum); 
+                sb.append("+\n");
+                stackNum++; 
+            }
+            if(stack.peek() == list[i]){ 
+                stack.pop();
+                sb.append("-\n");
+            }else{
+                System.out.println("NO");
+                return;
+            }
+        }
+        bw.write(sb.toString());
+        br.close();
+        bw.close();
+    }
+}
