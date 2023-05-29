d2553ea5c1fed6b60d4515edce3cc272ff566fd0
Jeonni <69911004+Jeonni@users.noreply.github.com>
2023-05-29 18:35:10 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 596 ms, Memory: 47496 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -20,10 +20,11 @@ public class Main {
     public static void hanoi(int n, int start, int mid, int end) {
         if (n == 1) {
             sb.append(start).append(" ").append(end).append("\n");
-        } else {
-            hanoi(n - 1, start, end, mid); // n-1개의 원판을 start 기둥에서 end 기둥을 거쳐 mid 기둥으로 옮기고,
-            sb.append(start).append(" ").append(end).append("\n"); // sb에 이동을 기록
-            hanoi(n - 1, mid, start, end); // 그 후 가장 큰 원판을 start 기둥에서 end 기둥으로 옮기고 다시 재귀적으로
+            return;
         }
+
+        hanoi(n - 1, start, end, mid);
+        sb.append(start).append(" ").append(end).append("\n");
+        hanoi(n - 1, mid, start, end);
     }
 }
